2ef592b690a4c890b74c6a9fca87fb28997d2a03
himinqq <139634770+himinqq@users.noreply.github.com>
2025-06-08 19:21:47 +0900

    [MG-24] feat : 별표 레이블 추가 시 보호 발신자 등록 API 및 삭제 방지 로직 구현 (#8)
    * [MG-24] refactor: sender 및 trash 로직 분리
    
    * [MG-24] feat : user_protected_sender 테이블 생성 (보호된 발신자 삭제 방지)
    
    * [MG-24] feat: 메일 별표 및 보호 발신자 등록/해제 기능 구현
    
    * [MG-24] feat: 보호된 발신자 메일 삭제 방지 로직 추가
    
    * [MG-24] feat: UserProtectedSender 모델 추가
    
    * [MG-24] refactor: 컨트롤러 라우터 등록 방식 변경
    
    * [MG-24] feat : 삭제 요청 스키마에 delete_protected_sender 옵션 추가
    
    * [MG-24] fix: 오류 발생 시 history_id 갱신하지 않도록 수정
    
    * [MG-24] refactor: 메일 배치 처리 재시도 및 대기 시간 로직 개선
    
    * [MG-24] fix: 삭제된 메일(is_deleted=True)이 조회 결과에 노출되는 문제 수정

---
+++ mailgreen/controller/__init__.py
@@ -0,0 +1,17 @@
+from .auth_controller import router as auth_router
+from .mail_controller import router as mail_router
+from .sender_controller import router as sender_router
+from .keyword_controller import router as keyword_router
+from .trash_controller import router as trash_router
+from .star_lable_controller import router as star_router
+from .carbon_controller import router as carbon_router
+
+__all__ = [
+    "auth_router",
+    "mail_router",
+    "sender_router",
+    "keyword_router",
+    "trash_router",
+    "star_router",
+    "carbon_router",
+]
